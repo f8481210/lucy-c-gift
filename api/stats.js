dcbbc5f9aa28e940b4dffb1b38ff93d744ab9542
f8481210 <59558992+f8481210@users.noreply.github.com>
2026-06-23 01:01:00 +0800

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,1 +1,31 @@
+import { promises as fs } from "fs";
 
+const file = "./data.json";
+
+async function readData() {
+    try {
+        const data = await fs.readFile(file, "utf8");
+        return JSON.parse(data);
+    } catch {
+        return { views: 0, runs: 0, copies: 0 };
+    }
+}
+
+async function saveData(data) {
+    await fs.writeFile(file, JSON.stringify(data));
+}
+
+export default async function handler(req, res) {
+
+    let data = await readData();
+
+    const type = req.query.type;
+
+    if (type === "view") data.views++;
+    if (type === "run") data.runs++;
+    if (type === "copy") data.copies++;
+
+    await saveData(data);
+
+    res.status(200).json(data);
+}
